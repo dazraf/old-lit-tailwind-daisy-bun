51fa26fd829d85666ec472265e9bc03b656a59de
dazraf <fuzz@dazraf.io>
2024-10-12 16:15:38 +0100

    Big improvement to ThemeElement - fixed the UI glitches and added more typesafety. Rationalised the app to focus only on the People's editing.

--- src/ThemeElement.ts
+++ src/ThemeElement.ts
@@ -2,41 +2,57 @@ import { customElement } from "lit/decorators.js";
 import { AppStyledElement } from "./AppStyledElement";
 import { html, css, PropertyValues } from "lit";
 
+type ThemeName = "default" | "light" | "dark" | "cupcake" | "night";
+
+type ThemeType = "light" | "dark" | undefined;
+
+interface ThemeDefinition {
+  title: string;
+  type: ThemeType;
+}
+
+type ThemeDefinitions = {
+  [K in ThemeName]: ThemeDefinition;
+};
+
+const _themes: ThemeDefinitions = {
+  default: {
+    title: "Default",
+    type: undefined,
+  },
+  light: {
+    title: "Light",
+    type: "light",
+  },
+  dark: {
+    title: "Dark",
+    type: "dark",
+  },
+  cupcake: {
+    title: "Cupcake",
+    type: "light",
+  },
+  night: {
+    title: "Night",
+    type: "dark",
+  },
+};
+
 @customElement("x-theme")
-export class ThemeElement extends AppStyledElement(css`
-  :host {
-    display: inline-block;
-    height: min-content;
-  }
-`) {
-  // accessor to get current theme from storage or default
-  static get theme() {
-    return localStorage.getItem("theme") || this.defaultTheme;
+export class ThemeElement extends AppStyledElement(css``) {
+  // -- static methods --
+
+  /**
+   * accessor to get current theme from storage or the browser default
+   */
+  static get currentTheme(): ThemeName {
+    return (localStorage.getItem("theme") as ThemeName) || this.defaultTheme;
   }
 
-  constructor() {
-    super();
-  }
-
-  override connectedCallback(): void {
-    super.connectedCallback();
-  }
-
-  protected firstUpdated(_changedProperties: PropertyValues): void {
-    this.theme = ThemeElement.theme;
-    super.firstUpdated(_changedProperties);
-  }
-
-  set theme(value: string) {
-    if (value === "default") {
-      value = ThemeElement.defaultTheme;
-    }
-    console.log("ThemeElement.set theme", value);
-    localStorage.setItem("theme", value);
-    this.shadowRoot?.ownerDocument.documentElement.setAttribute("data-theme", value);
-  }
-
-  static get defaultTheme() {
+  /**
+   * determine if the browser default theme is either light or dark
+   */
+  static get defaultTheme(): ThemeName {
     if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
       return "dark";
     } else {
@@ -44,76 +60,125 @@ export class ThemeElement extends AppStyledElement(css`
     }
   }
 
-  static get isLight() {
-    return this.theme === "light" || this.defaultTheme === "light";
+  static get isCurrentThemeLight() {
+    return _themes[this.currentTheme]?.type === "light";
+  }
+
+  // -- properties --
+
+  /**
+   * This is used to track the last selected theme. Used for cancelling a selection
+   */
+  private selectedTheme: string = ThemeElement.currentTheme;
+
+  // -- overrides --
+
+  override connectedCallback(): void {
+    // this is a more robust way of capturing click events on the dropdown
+    // e.g. when its embedded in a daisy menu, daisy will add a thick margin around the dropdown
+    // which while visible cannot be clicked on.
+    this.addEventListener("click", (e) => {
+      this.openDropDown();
+    });
+    super.connectedCallback();
+  }
+
+  protected firstUpdated(_changedProperties: PropertyValues): void {
+    this.documentTheme = ThemeElement.currentTheme;
+    super.firstUpdated(_changedProperties);
   }
 
   protected render() {
     return html`
-      <div class="dropdown h-min">
-        <div tabindex="0" role="button" class="btn m-1">
-          ${ThemeElement.isLight
+      <div
+        class="dropdown h-max w-max p-0 m-0"
+        @focusout=${this.closeDropDown}
+        @focusin=${this.onDropdownFocus}
+        @keydown=${this.onKey}
+        tabindex="0"
+      >
+        <div tabindex="0" role="button" class="m-0 p-0 border-0 border-spacing-y-0 max-h-min max-w-min">
+          ${ThemeElement.isCurrentThemeLight
             ? html`<x-icon icon="sun" filled></x-icon>`
             : html`<x-icon icon="moon" filled></x-icon>`}
-
-          <svg
-            width="12px"
-            height="12px"
-            class="inline-block h-2 w-2 fill-current opacity-60"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 2048 2048"
-          >
-            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
-          </svg>
         </div>
         <ul tabindex="0" class="dropdown-content bg-base-200 rounded-box z-[1] w-52 p-2 shadow">
-          <li>
-            <input
-              type="radio"
-              name="theme-dropdown"
-              class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-              aria-label="Default"
-              value="default"
-              @change=${this.onChangeTheme}
-            />
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="theme-dropdown"
-              class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-              aria-label="Light"
-              value="light"
-              @change=${this.onChangeTheme}
-            />
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="theme-dropdown"
-              class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-              aria-label="Dark"
-              value="dark"
-              @change=${this.onChangeTheme}
-            />
-          </li>
-          <li>
-            <input
-              type="radio"
-              name="theme-dropdown"
-              class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-              aria-label="Cupcake"
-              value="cupcake"
-              @change=${this.onChangeTheme}
-            />
-          </li>
+          ${this.renderThemes()}
         </ul>
       </div>
+      <div id="focus-here" class="w-0 h-0 p-0 m-0 absolute" tabindex="1"></div>
     `;
   }
+
+  private renderThemes() {
+    return Object.entries(_themes).map(
+      ([name, definition]) => html`
+        <li>
+          <input
+            type="radio"
+            name="theme-dropdown"
+            class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+            aria-label=${definition.title}
+            value=${name}
+            ?checked=${this.isThemeSelected(name)}
+            @change=${this.onChangeTheme}
+            @keydown=${this.onKey}
+          />
+        </li>
+      `
+    );
+  }
+
+  // -- event handlers --
+
+  private onKey(e: KeyboardEvent) {
+    if (e.code === "Escape" || e.code === "Enter") {
+      e.preventDefault();
+      if (e.code === "Escape") {
+        // reset
+        this.documentTheme = this.selectedTheme;
+      }
+      this.closeDropDown();
+      this.requestUpdate();
+    }
+  }
+
+  private onDropdownFocus() {
+    this.selectedTheme = ThemeElement.currentTheme;
+  }
+
   private onChangeTheme(e: Event) {
-    const theme = (e.target as HTMLInputElement).value;
-    this.theme = theme;
+    const theme = (e.target as HTMLInputElement).value as ThemeName;
+    this.documentTheme = theme;
     this.requestUpdate();
+  }
+
+  // -- private methods --
+
+  private openDropDown() {
+    const dropdown = this.renderRoot.querySelector(".dropdown") as HTMLElement;
+    const content = this.renderRoot.querySelector(".dropdown-content") as HTMLElement;
+    dropdown.classList.add("dropdown-open");
+    content.focus();
+  }
+
+  private closeDropDown() {
+    const dropdown = this.renderRoot.querySelector(".dropdown") as HTMLElement;
+    dropdown.classList.remove("dropdown-open");
+    const hiddenFocus = this.renderRoot.querySelector("#focus-here") as HTMLElement;
+    hiddenFocus.focus();
+    hiddenFocus.blur();
+  }
+
+  private isThemeSelected(theme: ThemeName) {
+    return ThemeElement.currentTheme === theme;
+  }
+
+  private set documentTheme(theme: ThemeName) {
+    if (theme === "default") {
+      theme = ThemeElement.defaultTheme;
+    }
+    localStorage.setItem("theme", theme);
+    this.shadowRoot?.ownerDocument.documentElement.setAttribute("data-theme", theme);
   }
 }
