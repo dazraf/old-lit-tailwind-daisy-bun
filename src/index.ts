93e6c975d558aff313645000d54cd7fd453ac08a
dazraf <fuzz@dazraf.io>
2024-10-21 13:58:08 +0100

    added handing of the URLPatter poly file and added Breadcrumbs component

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ import "./components/Application";
 import "./components/PeopleList";
 import "./components/ThemeSelector";
 import "./components/PersonEditor";
+import "./components/Breadcrumbs";
+
+// Conditional ESM module loading (Node.js and browser)
+// @ts-ignore: Property 'UrlPattern' does not exist
+if (!globalThis.URLPattern) {
+  await import("urlpattern-polyfill");
+}
